cb508a4186dd4600974170eb0b77f4adf0474db3
waltertick <ticktick.elvis@gmail.com>
2021-02-28 20:41:02 +0100

    templates statique du formulaire de modification d'un utilisateur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ExperienceListComponent } from './Shared/experience-list/experience-list.component';
 import { SigninComponent } from './Auth/signin/signin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,9 +6,14 @@ import { SignupComponent } from './Auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { SettingComponent } from './setting/setting.component';
 import { AuthGuardService } from './services/auth-guard.service';
+import { UserFormComponent } from './Shared/user-form/user-form.component';
+import { SkillFormComponent } from './Shared/skill-form/skill-form.component';
 
 const routes: Routes = [
   {path: 'auth/signin', component : SigninComponent},
+  {path: 'setting/user', component : UserFormComponent},
+  {path: 'setting/skill', component : SkillFormComponent},
+  {path: 'setting/experience', component : ExperienceListComponent},
   {path: 'auth/signup', component: SignupComponent},
   {path: 'home', canActivate: [AuthGuardService], component: HomeComponent},
   {path: 'setting', canActivate: [AuthGuardService], component: SettingComponent },
